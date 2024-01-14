ddac1889725806857e7c6a084f4e5f83af528345
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-01-15 00:08:57 +0100

    modification de la syntaxte et gerer les cas d'exception

--- programme/push_swap_tester.sh
+++ programme/push_swap_tester.sh
@@ -1,30 +1,7 @@
 #!/bin/sh
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    scypt.sh                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ilyanar <ilyanar@student.42lausanne.ch>    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/01/04 01:04:16 by ilyanar           #+#    #+#              #
-#    Updated: 2001/04/07 01:04:16 by ilyanar          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 numbers=""
 spinner="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
 delay=0.1
-
-echo "\033[94m╔════════════════════════════════════════════════════════════════════════╗\033[0m"
-echo "\033[94m║\033[0m               \033[95mWelcome to \033[0m\033[32;4mpush_swap\033[0m \033[95mtester, by ilyanar 🌸\033[0m  \033[94m	     	 ║\033[0m"
-echo "\033[94m╚════════════════════════════════════════════════════════════════════════╝\033[0m\n"
-
-sleep 0.1
-
-printf "\033[3;1;95mDo you want random numbers?\033[0m \033[32m<y/n>\033[0m \033[95m: \033[0m"
-read random
-printf "\033[3;1;95mwhich checker do you want to use?\033[0m \033[32m<1=original/2=bonus>\033[0m \033[95;1m: \033[0m"
-read check
 
 #fonctions
 
@@ -46,47 +23,120 @@ generate()
     	generate_unique_random
 	done
 }
-loading_animation() {
+
+loading_animation()
+{
+	local string=$1
     while true; do
         for char in $(echo "$spinner" | grep -o .); do
-            printf "\r\033[96m$char\033[0m"
+            printf "\r\033[96m$char $string\033[0m"
             sleep $delay
         done
     done
 }
+
+# scrypt beggening
+
+echo "\033[94m╔════════════════════════════════════════════════════════════════════════╗\033[0m"
+echo "\033[94m║\033[0m               \033[95mWelcome to \033[0m\033[32;4mpush_swap\033[0m \033[95mtester, by ilyanar 🌸\033[0m  \033[94m	     	 ║\033[0m"
+echo "\033[94m╚════════════════════════════════════════════════════════════════════════╝\033[0m\n"
+
+sleep 0.1
+
+# check push_swap file
+(loading_animation "checking files.") &
+loading_pid=$!
+sleep 1
+if ! [ -e "./push_swap" ]; then
+	printf "\033[1A\033[K"
+	printf "\n \033[31mpush_swap file does not exist.\033[0m"
+	kill -TERM "$loading_pid"
+	disown "$loading_pid"
+	wait "$loading_pid"
+	exit
+fi
+kill -TERM "$loading_pid"
+disown "$loading_pid"
+wait "$loading_pid"
+echo "\033[1A\033[K"
+#
+#
+#
+printf "\033[3;1;95mDo you want random numbers?\033[0m \033[32m<y/n>\033[0m \033[95m: \033[0m"
+read random
+echo "\033[2A\033[K"
+if [[ "$random" != "y" && "$random" != "n" ]]
+then
+	printf "\033[31mBad format.\033[0m"
+	exit
+fi
+printf "\033[3;1;95mwhich checker do you want to use?\033[0m \033[32m<1=original/2=bonus>\033[0m \033[95;1m: \033[0m"
+read check
+echo "\033[1A\033[K"
+echo "\033[2A\033[K"
+
+if [[ "$check" != "1" && "$check" != "2" ]]
+then
+	printf "\033[31mBad format.\033[0m"
+	exit
+fi
+
+if ! [ -e "./checker" ]; then
+	if [ "$check" != "1" ]; then
+		printf "\033[1A\033[K"
+		printf "\n\033[31mchecker file does not exist.\033[0m"
+		exit
+	fi
+fi
+#
+# check if files existe
+if ! [ -e "./push_swap" ]; then
+	printf "\033[31mBad format.\033[0m"
+	exit
+fi
+#
 # grep the random numbers
-# printf "\r\033[96m${spinner:$i:1}\033[0m \033[33msorting numbers...\033[0m"
 
 if [ "$random" == "n" ]
 then
 	echo "\033[94m════════════\033[0m\033[95mARGUMENTS\033[0m\033[94m════════════"
 	printf "\033[95mARG = \033[0m"
 	read numbers
-	echo "\033[94m════════════\033[0m\033[95mTIME\033[0m\033[94m════════════"
-	loading_animation &
+	(loading_animation "running push_swap...") &
 	loading_pid=$!
+	echo "\033[94m════════════\033[0m\033[95mTIME\033[0m\033[94m════════════"
 	sleep 1
 	LINE=$(time ./push_swap $numbers | wc -l)
 	kill -TERM "$loading_pid"
 	disown "$loading_pid"
 	wait "$loading_pid"
+	printf "\033[2A\033[K"
 elif [ "$random" == "y" ]
 then
 	printf "\033[3;1;95mHow many numbers you want : \033[0m"
 	read count
+	echo "\033[1A\033[K"
+	echo "\033[2A\033[K"
+	if ! [[ "$count" =~ ^[0-9]+$ ]]; then
+		printf "\033[31mBad format.\033[0m"
+		exit
+	fi
 	generate
-	loading_animation &
+	(loading_animation "running push_swap...") &
 	loading_pid=$!
 	echo "\n\033[94m════════════\033[0m\033[95mTIME\033[0m\033[94m════════════"
-	sleep 1
+	sleep 0.5
 	LINE=$(time ./push_swap $numbers | wc -l)
+	echo "\033[1A\033[K"
 	kill -TERM "$loading_pid"
 	disown "$loading_pid"
 	wait "$loading_pid"
+	printf "\033[2A\033[K"
 else
-	echo "Error bad format."
+	echo "\033[31mError bad format.\033[0m"
 	exit
 fi
+#
 # the checker
 
 if [ "$check" == "2" ]
@@ -95,11 +145,12 @@ then
 else
 	CHECK=$(./push_swap $numbers | ./srcs/checker_Mac $numbers)
 fi
+#
 # grep the time
-echo "\n\033[94m════════════\033[0m\033[95mOPERATIONS\033[0m\033[94m════════════\033[0m"
-echo "\033[92m"$LINE"\033[0m \033[32minstructions\033[0m"
+echo "\033[94m════════════\033[0m\033[95mOPERATIONS\033[0m\033[94m════════════\033[0m"
+echo "\033[92m"$LINE"\033[0m \033[32minstructions with $count numbers\033[0m"
 echo "\033[94m══════════════════════════════════\033[0m\n"
-
+#
 # check for checker
 
 if [ "$CHECK" == "OK" ]
@@ -110,19 +161,23 @@ then
 	echo "Checker say : ❌ [\033[31mKO\033[0m], Retry it :("
 elif [ "$CHECK" == "Error" ]
 then
-	echo "Checker say : ⛔️ \033[31;4mError\033[0m"
+	echo "\033[95mChecker say : \033[0m⛔️ \033[31;4mError\033[0m"
 else
 	echo "$CHECK bad format"
 fi
 
 echo ""
-
+#
 # print the stacks
 echo "\033[94m════════════════════════\033[0m"
 printf "\033[95mDo you want print the stack A and B?\033[0m \033[92m<y/n>\033[95m : \033[0m"
 read prints
 ARCH=$(sysctl -a | grep Intel)
-if [ "$prints" == "y" ]
+
+if [ "$(./push_swap $numbers)" == "Error" ]
+then
+	printf "\033[92mError\033[0m\n"
+elif [ "$prints" == "y" ]
 then
 	if [ ! "$ARCH" ]
 	then
@@ -131,18 +186,21 @@ then
 		./push_swap $numbers | ./srcs/print_mac_intel $numbers
 	fi
 fi
-
+#
 # print the instructions
 
 echo "\033[94m════════════════════════\033[0m"
 printf "\033[95mWant to see the instructions?\033[0m \033[92m<y/n>\033[95m : \033[0m"
 read intr
 
-if [ "$intr" == "y" ]
+if [ "$(./push_swap $numbers)" == "Error" ]
+then
+	printf "\033[92mError\033[0m\n"
+elif [ "$intr" == "y" ]
 then
 	./push_swap $numbers
 fi
-
+#
 #end
 #
 echo "\033[94m════════════════════════\033[0m"
